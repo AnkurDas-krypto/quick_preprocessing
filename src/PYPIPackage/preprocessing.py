2ff5eeaca3837919aea14d8c9038b2d13cb76185
Ankur Das <66483907+AnkurDas-krypto@users.noreply.github.com>
2022-12-19 00:12:08 -0500

    changes made in preprocessing.py

--- src/PYPIPackage/preprocessing.py
+++ src/PYPIPackage/preprocessing.py
@@ -14,27 +14,24 @@ from nltk.stem import WordNetLemmatizer
 
 
 @ensure_annotations
-def text_preprocessing(main_list: list) -> list:
+def text_preprocessing(main_list: list):
     try:
         corpus = []
         if main_list is None:
             raise InvalidListException("list cannot be null")
-        elif type(main_list) is not list:
-            raise InvalidListException("input should be in list format")
         else:
             lemmatizer = WordNetLemmatizer()
-            for i in range(len(main_list)):
+            for i in range(0, len(main_list)):
                 review = re.sub('[^a-zA-Z]', ' ', main_list[i])
-                logger.info(f"removing punctuations: {review}")
+                #logger.info(f"removing punctuations: {review}")
                 review = review.lower()
-                logger.info(f"lowering the sentence: {review}")
-                review = review.split()
-                review = [lemmatizer.lemmatize(word) for word in review if not word in stopwords.words('english')]
-                logger.info(f"lemmatizing each words: {review}")
-                review = ' '.join(review)
+                #logger.info(f"lowering the sentence: {review}")
+               # review = review.split()
+                #review = [lemmatizer.lemmatize(word) for word in review if word not in stopwords.words('english')]
+                #logger.info(f"lemmatizing each words: {review}")
+                review = ' '.join([lemmatizer.lemmatize(word) for word in review.split() if word not in stopwords.words('english')])
                 corpus.append(review)
                 logger.info(f"successfully preprocessed")
         return corpus
     except Exception as e:
         raise e
-
